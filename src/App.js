b17020bead96f286e307a89698771ccb02e730c4
Rick Beyer <beyer.richard73@gmail.com>
2021-10-04 22:51:14 -0500

    Marker of ISS now moves with updated latitude and longitude.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import ISSInfo from './components/ISSInfo';
 import { useState } from 'react';
 import { useEffect } from 'react/cjs/react.development';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
   const [data, setData] = useState('');
-  const [pos, setPos] = useState([0,0]);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
   const [dataReady, setDataReady] = useState(false);
-  
+    
   const getData = async function getISSData() {
     const iss_info = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
     const iss_data = await iss_info.json();
@@ -23,11 +23,16 @@ function App() {
 
   useEffect(() => {
     if(data) {
-        const thisPos = [data.latitude, data.longitude];
+      const thisPos = [data.latitude, data.longitude];
 
-        setPos(tmp => [...thisPos]);
+      setLat(data.latitude);
+      setLon(data.longitude);
 
-        setDataReady(true);
+      setDataReady(true);
+
+      setTimeout(() => {
+        getData();
+      }, 2000);
     }
 
   }, [data]);
@@ -35,13 +40,16 @@ function App() {
   return (
     <div className="App">
       <img src="https://www.nasa.gov/sites/default/files/styles/card_page_banner/public/thumbnails/image/iss_banner_webage_20191030.jpg" alt="International Space Station" />
-      <ISSInfo data={data} />
-      {dataReady ? <MapContainer style={{ height: '500px', width: '500px' }} center={pos} zoom={5} scrollWheelZoom={true}>
+      {dataReady ? <MapContainer
+        style={{ height: '500px', width: '800px' }}
+        center={[lat, lon]}
+        zoom={5}
+        scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[14.679283800932, 119.15183257647]}>
+          <Marker position={[lat, lon]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
